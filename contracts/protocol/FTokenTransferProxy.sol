9aabbffdc969cb9006db0a8eca3dc1dc67195b9f
xpeper <xpepermint@gmail.com>
2018-05-31 11:27:33 +0200

    Add swapper and proxy template

---
+++ contracts/protocol/FTokenTransferProxy.sol
@@ -0,0 +1,96 @@
+// pragma solidity 0.4.23;
+
+// import "../tokens/ERC20.sol";
+// import "../ownership/Ownable.sol";
+
+// /// @title TokenTransferProxy - Transfers tokens on behalf of contracts that have been approved via decentralized governance.
+// /// @author Amir Bandeali - <amir@0xProject.com>, Will Warren - <will@0xProject.com>
+// contract TokenTransferProxy is Ownable {
+
+//     /// @dev Only authorized addresses can invoke functions with this modifier.
+//     modifier onlyAuthorized {
+//         require(authorized[msg.sender]);
+//         _;
+//     }
+
+//     modifier targetAuthorized(address target) {
+//         require(authorized[target]);
+//         _;
+//     }
+
+//     modifier targetNotAuthorized(address target) {
+//         require(!authorized[target]);
+//         _;
+//     }
+
+//     mapping (address => bool) public authorized;
+//     address[] public authorities;
+
+//     event LogAuthorizedAddressAdded(address indexed target, address indexed caller);
+//     event LogAuthorizedAddressRemoved(address indexed target, address indexed caller);
+
+//     /*
+//      * Public functions
+//      */
+
+//     /// @dev Authorizes an address.
+//     /// @param target Address to authorize.
+//     function addAuthorizedAddress(address target)
+//         public
+//         onlyOwner
+//         targetNotAuthorized(target)
+//     {
+//         authorized[target] = true;
+//         authorities.push(target);
+//         emit LogAuthorizedAddressAdded(target, msg.sender);
+//     }
+
+//     /// @dev Removes authorizion of an address.
+//     /// @param target Address to remove authorization from.
+//     function removeAuthorizedAddress(address target)
+//         public
+//         onlyOwner
+//         targetAuthorized(target)
+//     {
+//         delete authorized[target];
+//         for (uint i = 0; i < authorities.length; i++) {
+//             if (authorities[i] == target) {
+//                 authorities[i] = authorities[authorities.length - 1];
+//                 authorities.length -= 1;
+//                 break;
+//             }
+//         }
+//         emit LogAuthorizedAddressRemoved(target, msg.sender);
+//     }
+
+//     /// @dev Calls into ERC20 Token contract, invoking transferFrom.
+//     /// @param token Address of token to transfer.
+//     /// @param from Address to transfer token from.
+//     /// @param to Address to transfer token to.
+//     /// @param value Amount of token to transfer.
+//     /// @return Success of transfer.
+//     function transferFrom(address token,
+//                           address from,
+//                           address to,
+//                           uint value)
+//       public
+//       onlyAuthorized
+//       returns (bool)
+//     {
+//       return ERC20(token).transferFrom(from, to, value);
+//     }
+
+//     /*
+//      * Public constant functions
+//      */
+
+//     /// @dev Gets all authorized addresses.
+//     /// @return Array of authorized addresses.
+//     function getAuthorizedAddresses()
+//         public
+//         constant
+//         returns (address[])
+//     {
+//         return authorities;
+//     }
+// }
---
+++ contracts/protocol/NFTokenTransferProxy.sol
@@ -0,0 +1,94 @@
+// pragma solidity 0.4.23;
+
+// /*
+//  * @dev based od: https://github.com/0xProject/contracts/blob/master/contracts/TokenTransferProxy.sol
+//  */
+
+
+// import "../tokens/ERC721.sol";
+// import "../ownership/Ownable.sol";
+
+// /// @title NFTokenTransferProxy - Transfers none-fundgible tokens on behalf of contracts that have been approved via decentralized governance.
+// contract NFTokenTransferProxy is Ownable {
+
+//   /// @dev Only authorized addresses can invoke functions with this modifier.
+//   modifier onlyAuthorized {
+//     require(authorized[msg.sender]);
+//     _;
+//   }
+
+//   modifier targetAuthorized(address target) {
+//     require(authorized[target]);
+//     _;
+//   }
+
+//   modifier targetNotAuthorized(address target) {
+//     require(!authorized[target]);
+//     _;
+//   }
+
+//   mapping (address => bool) public authorized;
+//   address[] public authorities;
+
+//   event LogAuthorizedAddressAdded(address indexed target, address indexed caller);
+//   event LogAuthorizedAddressRemoved(address indexed target, address indexed caller);
+
+//   /*
+//    * Public functions
+//    */
+
+//   /// @dev Authorizes an address.
+//   /// @param target Address to authorize.
+//   function addAuthorizedAddress(address target)
+//     public
+//     onlyOwner
+//     targetNotAuthorized(target)
+//   {
+//     authorized[target] = true;
+//     authorities.push(target);
+//     emit LogAuthorizedAddressAdded(target, msg.sender);
+//   }
+
+//   /// @dev Removes authorizion of an address.
+//   /// @param target Address to remove authorization from.
+//   function removeAuthorizedAddress(address target)
+//     public
+//     onlyOwner
+//     targetAuthorized(target)
+//   {
+//     delete authorized[target];
+//     for (uint i = 0; i < authorities.length; i++) {
+//         if (authorities[i] == target) {
+//             authorities[i] = authorities[authorities.length - 1];
+//             authorities.length -= 1;
+//             break;
+//         }
+//     }
+//     emit LogAuthorizedAddressRemoved(target, msg.sender);
+//   }
+
+//   function transferFrom(
+//     address _nfToken,
+//     address _from,
+//     address _to,
+//     uint256 _id)
+//     external
+//     onlyAuthorized
+//   {
+//     ERC721(_nfToken).transferFrom(_from, _to, _id);
+//   }
+
+//   /*
+//    * Public constant functions
+//    */
+
+//   /// @dev Gets all authorized addresses.
+//   /// @return Array of authorized addresses.
+//   function getAuthorizedAddresses()
+//     public
+//     constant
+//     returns (address[])
+//   {
+//     return authorities;
+//   }
+// }

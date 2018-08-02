8a672061db2985079113b2d450a4a5c9be0e1274
Tadej Vengust <vengust.tadej@gmail.com>
2018-08-02 13:26:51 +0200

    Add proxy test wireframes.

---
+++ src/tests/proxy/tokens.test.ts
@@ -0,0 +1,29 @@
+import { Spec } from '@specron/spec';
+
+const spec = new Spec();
+
+spec.test('adds authorized address', async (ctx) => {
+  
+});
+
+spec.test('fails when trying to add an already authorized address', async (ctx) => {
+  
+});
+
+spec.test('removes authorized address', async (ctx) => {
+  
+});
+
+spec.test('fails when trying to remove an already unauthorized address', async (ctx) => {
+  
+});
+
+spec.test('transfers tokens', async (ctx) => {
+  
+});
+
+spec.test('fails if transfer is triggered by an unauthorized address', async (ctx) => {
+  
+});
+
+export default spec;

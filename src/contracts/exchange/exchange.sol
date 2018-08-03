1df1d53972cebcbed97f084e420c660a59814a32
Tadej Vengust <vengust.tadej@gmail.com>
2018-08-03 08:34:16 +0200

    Dex contract init.

---
+++ src/contracts/exchange/exchange.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @dev Decentralize exchange for fundible and non-fundgible tokens powered by atomic swaps. 
+ */
+contract Exchange {
+  
+  constructor () 
+    public
+  {
+
+  }
+}

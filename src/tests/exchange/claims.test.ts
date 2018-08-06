91840752be34dbee66ef71d7a50b6c0406c65de1
Tadej Vengust <vengust.tadej@gmail.com>
2018-08-06 15:25:30 +0200

    Add tests for isValidSignature.

--- src/tests/exchange/claims.test.ts
+++ src/tests/exchange/claims.test.ts
@@ -15,39 +15,79 @@ interface Data {
   account9: string;
 }
 
-const signedClaim = new Spec<Data>();
+interface SignatureData extends Data {
+  signature: any;
+  hash: string;
+}
+
+const signedClaim = new Spec<SignatureData>();
+
+signedClaim.beforeEach(async (ctx) => {
+  const claim = {
+    maker: ctx.get('account2'),
+    taker: ctx.get('account3'),
+    transfers: [
+      {
+        token: ctx.get('cat')._address,
+        kind: 1,
+        from: ctx.get('account2'),
+        to: ctx.get('account3'),
+        value: 1
+      }
+    ],
+    seed: new Date().getTime(), 
+    expiration: new Date().getTime() + 600
+  }
+
+  const exchange = ctx.get('exchange');
+  const hash = await exchange.methods.getSwapDataClaim(ctx.toTuple(claim)).call();
+  ctx.set('hash', hash);
+  const signature = await ctx.web3.eth.sign(hash, ctx.get('account2'));
+  const signatureData = {
+    r: signature.substr(0, 66),
+    s: '0x' + signature.substr(66, 64),
+    v: parseInt('0x' + signature.substr(130, 2)) + 27,
+    kind: 0
+  }
+  ctx.set('signature', signatureData);
+});
 
 signedClaim.test('with valid signature data', async (ctx) => {
-  
+  const exchange = ctx.get('exchange');
+  const valid = await exchange.methods
+    .isValidSignature(
+      ctx.get('account2'),
+      ctx.get('hash'),
+      ctx.toTuple(ctx.get('signature'))
+    ).call();
+  ctx.true(valid);
 });
 
 signedClaim.test('with invalid signature data', async (ctx) => {
-  
+  const exchange = ctx.get('exchange');
+  const signatureData = ctx.get('signature');
+  signatureData.v = 30;
+  const valid = await exchange.methods
+    .isValidSignature(
+      ctx.get('account2'),
+      ctx.get('hash'),
+      ctx.toTuple(signatureData)
+    ).call();
+  ctx.false(valid);
 });
 
 signedClaim.test('from a third party account', async (ctx) => {
-  
+  const exchange = ctx.get('exchange');
+  const valid = await exchange.methods
+    .isValidSignature(
+      ctx.get('account3'),
+      ctx.get('hash'),
+      ctx.toTuple(ctx.get('signature'))
+    ).call();
+  ctx.false(valid);
 });
 
 const rawClaim = new Spec<Data>();
-
-rawClaim.beforeEach(async (ctx) => {
-
-  const catNFT = await ctx.requireContract(
-    { 
-      src: './node_modules/@0xcert/ethereum-erc721/build/contracts/NFTokenMetadataEnumerableMock.json',
-      args: ['cat', 'CAT'],
-      gas: 4000000
-    }
-  );
-
-  ctx.set('cat', catNFT);
-
-  await catNFT.methods
-    .mint(ctx.get('account2'), 1, 'http://0xcert.org')
-    .send({from: ctx.get('owner'), gas: 4000000});
-
-});
 
 rawClaim.test('from valid data', async (ctx) => {
 
@@ -70,7 +110,7 @@ rawClaim.test('from valid data', async (ctx) => {
   const exchange = ctx.get('exchange');
   const hash = await exchange.methods.getSwapDataClaim(ctx.toTuple(claim)).call();
 
-  // TODO(Tadej): generate hash locally an compare.
+  // TODO(Tadej): generate hash locally and compare.
 
   // ctx.is(hash, );
 });
@@ -94,6 +134,20 @@ spec.beforeEach(async (ctx) => {
   ctx.set('account7', accounts[7]);
   ctx.set('account8', accounts[8]);
   ctx.set('account9', accounts[9]);
+
+  const catNFT = await ctx.requireContract(
+    { 
+      src: './node_modules/@0xcert/ethereum-erc721/build/contracts/NFTokenMetadataEnumerableMock.json',
+      args: ['cat', 'CAT'],
+      gas: 4000000
+    }
+  );
+
+  ctx.set('cat', catNFT);
+
+  await catNFT.methods
+    .mint(ctx.get('account2'), 1, 'http://0xcert.org')
+    .send({from: ctx.get('owner'), gas: 4000000});
 });
 
 spec.spec('generate claim', rawClaim);

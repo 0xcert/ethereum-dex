71836d23d4d4d424d7abd98e90d2ba9cb21d3fc9
Tadej Vengust <vengust.tadej@gmail.com>
2018-08-20 15:41:58 +0200

    Add tests for proxies.

--- src/tests/exchange/proxies.test.ts
+++ src/tests/exchange/proxies.test.ts
@@ -1,12 +1,84 @@
 import { Spec } from '@specron/spec';
 
-const spec = new Spec();
+/**
+ * Spec context interfaces.
+ */
+
+interface Data {
+  tokenProxy?: any;
+  nftProxy?: any;
+  zeroAddress?: string;
+  randomAddress?: string;
+}
+
+const spec = new Spec<Data>();
+
+spec.beforeEach(async (ctx) => {
+  const tokenProxy = await ctx.deploy({
+    src: './build/token-transfer-proxy.json',
+    contract: 'TokenTransferProxy'
+  });
+  ctx.set('tokenProxy', tokenProxy);
+});
+
+spec.beforeEach(async (ctx) => {
+  const nftProxy = await ctx.deploy({
+    src: './build/nftokens-transfer-proxy.json',
+    contract: 'NFTokenTransferProxy',
+  });
+  ctx.set('nftProxy', nftProxy);
+});
+
+spec.beforeEach(async (ctx) => {
+  const zeroAddress = '0x0000000000000000000000000000000000000000';
+  const randomAddress = '0x0000000000000000000000000000000000000001';
+  ctx.set('zeroAddress', zeroAddress);
+  ctx.set('randomAddress', randomAddress);
+});
 
 spec.test('deploy exchange with correct ERC20 transfer proxy address', async (ctx) => {
-  
+  const tokenProxy = ctx.get('tokenProxy');
+  const zeroAddress = ctx.get('zeroAddress');
+  const randomAddress = ctx.get('randomAddress');
+
+  await ctx.reverts(() => ctx.deploy({
+    src: './build/exchange.json',
+    contract: 'Exchange',
+    args: [zeroAddress, randomAddress]
+  }));
+
+  const exchange = await ctx.deploy({
+    src: './build/exchange.json',
+    contract: 'Exchange',
+    args: [tokenProxy._address, randomAddress]
+  });
+
+  const exhangeTokenProxyAddress = await exchange.methods.tokenTransferProxy().call();
+
+  ctx.is(tokenProxy._address, exhangeTokenProxyAddress);
 });
 
 spec.test('deploy exchange with correct ERC721 transfer proxy address', async (ctx) => {
+
+  const nftProxy = ctx.get('nftProxy');
+  const zeroAddress = ctx.get('zeroAddress');
+  const randomAddress = ctx.get('randomAddress');
+
+  await ctx.reverts(() => ctx.deploy({
+    src: './build/exchange.json',
+    contract: 'Exchange',
+    args: [randomAddress, zeroAddress]
+  }));
+
+  const exchange = await ctx.deploy({
+    src: './build/exchange.json',
+    contract: 'Exchange',
+    args: [randomAddress, nftProxy._address]
+  });
+
+  const exhangeNFTokenProxyAddress = await exchange.methods.nfTokenTransferProxy().call();
+
+  ctx.is(nftProxy._address, exhangeNFTokenProxyAddress);
   
 });
 
